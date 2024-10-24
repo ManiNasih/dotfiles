07ae075746da97d16e83eadaf76be8c3c4eef8fe
ManiNasih <w5824805@gmail.com>
2024-10-24 11:02:12 +0300

    make managing dotfiles simpler

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  -- {
+  --   "catppuccin",
+  --   opts = {
+  --     transparent_background = true,
+  --   },
+  -- },
+
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   -- opts = {
+  --   -- transparent = true,
+  --   -- styles = {
+  --   -- floats = "transparent",
+  --   -- sidebars = "transparent",
+  --   -- },
+  --   -- },
+  -- },
+}
